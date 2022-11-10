5a210eca63120a12dd1c9523ca7ec4716ebd241e
joaqu <joaqu@LAPTOP-LPN88GB9>
2022-11-10 19:21:30 +0100

    Git init

---
+++ Maven/src/main/java/ISOC04/Maven/App.java
@@ -0,0 +1,13 @@
+package ISOC04.Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
