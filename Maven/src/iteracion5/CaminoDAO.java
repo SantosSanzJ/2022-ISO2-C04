6804c76165c1d1ca99efcca9bc7d44376a4f81b9
CristianRubio4 <Cristian.Rubio@alu.uclm.es>
2022-12-09 17:24:48 +0100

    Finalizada iteracion5

---
+++ Maven/src/iteracion5/CaminoDAO.java
@@ -0,0 +1,19 @@
+package Diseño;
+
+public class CaminoDAO {
+
+	/**
+	 * 
+	 * @param camino
+	 */
+	public String informacion(Camino camino) {
+		// TODO - implement CaminoDAO.informacion
+		throw new UnsupportedOperationException();
+	}
+
+	public Camino[] escogerCaminos() {
+		// TODO - implement CaminoDAO.escogerCaminos
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ Maven/src/iteracion5/GestorCamino.java
@@ -0,0 +1,16 @@
+package Diseño;
+
+public class GestorCamino {
+
+	/**
+	 * 
+	 * @param lugar
+	 * @param nombreRuta
+	 * @param idInicio
+	 */
+	public Camino[] informacionCamino(String lugar, String nombreRuta, int idInicio) {
+		// TODO - implement GestorCamino.informacionCamino
+		throw new UnsupportedOperationException();
+	}
+
+}
